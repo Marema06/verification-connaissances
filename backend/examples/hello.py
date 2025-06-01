828ffcde89ce32600fa97ce919544aae26bd04c7
Marema06 <msarr0938@gmail.com>
2025-06-01 17:11:55 +0200

    CI: add full workflow

---
+++ backend/examples/hello.py
@@ -0,0 +1,7 @@
+def saluer(nom):
+    message = f"Bonjour, {nom} !"
+    print(message)
+    return message
+
+if __name__ == "__main__":
+    saluer("Marieme")
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://lately-noble-tuna.ngrok-free.app',
+  apiUrl: 'https://c865-134-59-155-253.ngrok-free.app',
   apiToken: '2xHtqn7INtJHBIKwcVFmYXXMUUm_8pGB3aQe17uuMCaTRXVh'
 };
