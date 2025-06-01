ec1fd633e56686b1cef0eafd87c55a53046dddf9
Marema06 <msarr0938@gmail.com>
2025-06-01 18:57:34 +0200

    test

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://c865-134-59-155-253.ngrok-free.app',
+  apiUrl: 'https://7a14-46-193-6-178.ngrok-free.app',
   apiToken: '2xHtqn7INtJHBIKwcVFmYXXMUUm_8pGB3aQe17uuMCaTRXVh'
 };
