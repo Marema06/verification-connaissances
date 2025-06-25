e157e657fda2158b81016f0d7a257de36293d9e7
Marema06 <msarr0938@gmail.com>
2025-06-25 08:13:37 +0200

    modification d'n fichier

--- test_auto.py
+++ test_auto.py
@@ -1,12 +1,4 @@
-def est_pair(nombre):
-    if nombre % 2 == 0:
-        return True
-    else:
-        return False
+def addition(a, b):
+    return a + b
 
-# Exemple d'utilisation
-n = 7
-if est_pair(n):
-    print(f"{n} est pair.")
-else:
-    print(f"{n} est impair.")
+print(addition(8, 1))
